ecb9d0a7c2982405725abf953272064d703014f4
Akkilesh-A <akkilalagar05@gmail.com>
2024-11-21 21:02:22 +0530

    feat: add leetcode badge

--- components/homepageSections/Badges.tsx
+++ components/homepageSections/Badges.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import VortexWrapper from '../VortexWrapper';
-import { badgesData, certificatesData } from '@/data';
+import { badgesData } from '@/data';
 import Image from 'next/image';
 import { Heading } from '../ui';
 import { FaExternalLinkAlt } from 'react-icons/fa';
@@ -13,44 +13,16 @@ const Badges = () => {
             <div className='flex flex-col sm:flex-row items-center justify-center flex-wrap gap-16'>
                 {badgesData.map(({href,name,description,fileName},index)=>{
                     return(
-                        <div key={index} className="rounded-[22px] sm:max-w-[25vw] max-w-[60vw] p-4 sm:p-10 bg-gray-400 bg-opacity-10">
-                            <Image
-                            src={`/badges/${fileName}`}
-                            alt="jordans"
-                            height="200"
-                            width="200"
-                            className="object-contain"
-                            />
-                            <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-                            {name}
-                            </p>
-                    
-                            <p className="text-sm text-neutral-600 dark:text-neutral-400">
-                            {description}
-                            </p>
-                            <a href={href}>
-                                <button className="rounded-full pl-4 pr-1 py-1 text-white flex gap-2 items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-                                    <span>Verify</span>
-                                    <span className="bg-zinc-700 rounded-full text-[0.6rem] px-4 py-3 text-white">
-                                        <FaExternalLinkAlt />
-                                    </span>
-                                </button>
-                            </a>
-                        </div>
-                    )
-                })}
-            </div>   
-            <div className='flex flex-col sm:flex-row items-center justify-center flex-wrap gap-16'>
-                {certificatesData.map(({href,name,description,fileName},index)=>{
-                    return(
-                        <div key={index} className="rounded-[22px] sm:max-w-[25vw] max-w-[60vw] p-4 sm:p-10 bg-gray-400 bg-opacity-10">
-                            <Image
-                            src={`/certificates/${fileName}`}
-                            alt="jordans"
-                            height="200"
-                            width="300"
-                            className="object-contain"
-                            />
+                        <div key={index} className="rounded-xl w-[80vw] p-4 sm:p-10 bg-gray-400 bg-opacity-10">
+                            <div className='flex justify-center'>
+                                <Image
+                                src={`/${fileName}`}
+                                alt={name}
+                                height="200"
+                                width="250"
+                                className="object-contain"
+                                />
+                            </div>
                             <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
                             {name}
                             </p>
--- data/BadgesData.ts
+++ data/BadgesData.ts
@@ -1,23 +1,26 @@
 export const badgesData=[
     {
         href: "https://badgr.com/public/assertions/4I39DxpuS0SecNwWquafDw?identity__email=akkilalagar05@gmail.com",
-        name: "Postman API Student Expert",
+        name: "Postman API",
         description: "Postman Student Expert Badge",
-        fileName:"PostmanAPIFundamentals.png"
+        fileName:"badges/PostmanAPIFundamentals.png"
     },
     {
         href: "https://x.com/defy_2024/status/1750560492912087180/photo/1",
-        name: "DeFy'24",
+        name: "DeFy'24 - Champions",
         description: "A 24 hour web3 hackathon",
-        fileName:"DeFy'24.png"
-    }
-]
-
-export const certificatesData=[
+        fileName:"badges/DeFy'24.png"
+    },
     {
         href: "ude.my/UC-fca5d6a5-2035-4ce6-9072-a47e2aa828a8",
         name: "Web Dev Bootcamp",
         description: "Web Development Bootcamp 2024 ",
-        fileName:"WebDevBootcamp.jpg"
+        fileName:"certificates/WebDevBootcamp.jpg"
+    },
+    {
+        href:"https://leetcode.com/u/Akkilesh_A/",
+        name:"LeetCode",
+        description:"LeetCode Problem Solving",
+        fileName:"badges/LeetCode50.gif"
     }
 ]
